e2668a5f9ff6f197aa4e4e284559d2e60de4b0d9
MarianaSouza <mariana.desouza@pixelunion.net>
2021-05-30 21:42:08 -0700

    Add gitignore

--- node_modules/next/dist/lib/eslint/getLintIntent.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type LintIntent = {
-    firstTimeSetup: boolean;
-};
-export declare function getLintIntent(eslintrcFile: string | null, pkgJsonEslintConfig: string | null): Promise<LintIntent | false>;
--- node_modules/next/dist/next-server/server/config-utils-worker.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function install(useWebpack5: boolean): void;
-export declare type CheckReasons = 'test-mode' | 'no-config' | 'future-flag' | 'no-future-flag' | 'no-webpack-config' | 'webpack-config';
-export declare type CheckResult = {
-    enabled: boolean;
-    reason: CheckReasons;
-};
-export declare function shouldLoadWithWebpack5(phase: string, dir: string): Promise<CheckResult>;
