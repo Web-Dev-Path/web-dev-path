1e7929a4df9df857856373e6acc3702516860138
Zahra <zbehfarshad@gmail.com>
2021-08-18 11:14:19 -0700

    Deleting the Home.module.css file, creating Home.module.scss file that contains all the necessary styles for Home page in SASS format and referencing the scss file in Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
