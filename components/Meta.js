7e0ef91c5e01dc959a264aff4fad9bdb488c7d49
rebecatschiedel <rebecatcmg@gmail.com>
2021-11-12 15:52:39 -0800

    Change import from utils

--- components/Meta.js
+++ components/Meta.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
-import { title, keywords, description } from "../utils/meta";
+import { meta } from "../utils/meta";
 
 export default function Meta() {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="keywords" content={keywords} />
-      <meta name="description" content={description} />
+      <meta name="keywords" content={meta.keywords} />
+      <meta name="description" content={meta.description} />
       <meta charSet="utf-8" />
-      <title>{title}</title>
+      <title>{meta.title}</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
