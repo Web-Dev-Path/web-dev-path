a6f12d85722a8ec3b99c85a2eb23abfb7fee7dc0
Muammer Necipoglu <muammernecipoglu@gmail.com>
2022-02-07 21:58:41 -0800

    Add styling to footer for mobile and tablet view

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import footerStyles from '../styles/Footer.module.scss';
-import { linksNav, linksSocial } from '../utils/links';
+import { linksNav } from '../utils/links';
 import NewsletterSubscribe from './mailchimp/NewsletterSubscribe';
 import Image from 'next/image';
 
@@ -8,6 +8,18 @@ export default function Footer() {
   return (
     <footer className={footerStyles.footer}>
       <NewsletterSubscribe />
+      <section className={footerStyles.footer__section}>
+        <nav className={footerStyles.footer__nav} aria-label="Main">
+          <ul className={footerStyles.footer__navList}>
+            {linksNav.map(link => (
+              <li className={footerStyles.footer__navItem}>
+                <Link href={link.href}>{link.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Image src="/../public/images/svg/logo.svg" height={326} width={326} />
+      </section>
     </footer>
   );
 }
