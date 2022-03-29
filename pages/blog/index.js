ae3f2473bdc563bf7c35ef91330e2f0a43796a45
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2022-03-28 22:22:50 -0700

    Adding temporary styles to pages in development

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,8 +2,9 @@ import Link from 'next/link';
 
 export default function Blog() {
   return (
-    <div>
-      <h1>Hello from the blog page</h1>
+    <div className="blog">
+      <h1>Blog Page</h1>
+      <p>We're building this page. Stay tuned!</p>
       <ul>
         <li>
           <Link href="/blog/first-post">
