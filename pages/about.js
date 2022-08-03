843b72f77041b196589f9c79723123ba4442eada
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-08-03 11:17:26 +0400

    set up content and svgs

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import RevealContentContainer from '@/components/containers/RevealContentContainer';
-import Title from '@/components/snippets/Title';
+// import Title from '@/components/snippets/Title';
 import TwoColumn from '@/components/containers/TwoColumn';
 import { white, primary, primaryAccent } from '@/styles/TwoColumn.module.scss';
 import Container from '@/components/containers/Container';
@@ -10,45 +10,39 @@ export default function AboutUs() {
       <RevealContentContainer>
         {/* ================================OUR BACKGROUND SECTION============== */}
         {/* <Container> */}
-          {/* <Title title='Our background' /> */}
-          <TwoColumn
-          title="Our background"
-            content={
-              <div>
-                The Web Dev Path project was idealized in 2020 in consequence of
-                many conversations with aspirant web developers as well as by
-                considering some personal experiences.
-                <br />
-                <br />
-                When talking to other junior developers, it was evident the lack
-                of practical experience with version control in a team dynamic
-                was making their integration in a professional environment
-                harder.
-              </div>
-            }
-            rowOrder='row'
-            image='/images/svg/square-brackets.svg'
-            color={primary}
-            bgColor={primaryAccent}
-            customInnerClass='our-background'
-          />
+        {/* <Title title='Our background' /> */}
+        <TwoColumn
+          title='Our background'
+          content={
+            <div>
+              The Web Dev Path project was idealized in 2020 in consequence of
+              many conversations with aspirant web developers as well as by
+              considering some personal experiences.
+              <br />
+              <br />
+              When talking to other junior developers, it was evident the lack
+              of practical experience with version control in a team dynamic was
+              making their integration in a professional environment harder.
+            </div>
+          }
+          rowOrder='row'
+          image='/images/svg/square-brackets.svg'
+          color={primary}
+          bgColor={primaryAccent}
+          customInnerClass='our-background'
+        />
         {/* </Container> */}
         {/* ================================OUR BACKGROUND SECTION============== */}
       </RevealContentContainer>
 
       <RevealContentContainer>
         <Container>
-          <Title title='About Web Dev Path' />
+          {/* <Title title='Peer reviews' /> */}
           <TwoColumn
+            title='Peer reviews'
             content={
               <div>
-                The Web Dev Path project was idealized in 2020 in consequence of
-                many conversations with aspirant web developers as well as by
-                considering some personal experiences. When talking to other
-                junior developers, it was evident the lack of practical
-                experience with version control in a team dynamic was making
-                their integration in a professional environment harder. Most of
-                those new graduates used a version control feature, such
+                Most of those new graduates used a version control feature, such
                 as&nbsp;
                 <a
                   target='_blank'
@@ -67,32 +61,37 @@ export default function AboutUs() {
                 </a>
                 , as a repository for their personal projects where they were
                 the only participants. So, all that “branch out and PR review”
-                enriching process was completely unknown to them. That happened
-                to be a problem either for being considered for “junior”
-                positions and after being hired because, in most cases, their
-                team lead was a very, very busy senior developer…
+                enriching process was completely unknown to them.
+                <br />
+                <br />
+                That happened to be a problem either for being considered for
+                “junior” positions and after being hired because, in most cases,
+                their team lead was a very, very busy senior developer…
               </div>
             }
             rowOrder='row-reverse'
-            image='/images/svg/square-brackets.svg'
+            image='/images/svg/open-angle-bracket.svg'
             color={primary}
             bgColor={white}
             customInnerClass='about-content'
           />
           <TwoColumn
+            title='Version control'
             content={
               <div>
                 Another problem found out is that collaborating with open-source
                 projects, the way it is possible to gain some version control
                 experience, can be quite intimidating since there isn’t a clear
-                mentoring aspect. It can take years for someone to develop “a
-                logic mindset” that allows solving a problem by using a
-                programming language, so just going to an open-source project
-                with a list of available tasks that don’t make any sense won’t
-                cut it.
+                mentoring aspect.
+                <br />
+                <br />
+                It can take years for someone to develop “a logic mindset” that
+                allows solving a problem by using a programming language, so
+                just going to an open-source project with a list of available
+                tasks that don’t make any sense won’t cut it.
               </div>
             }
-            image='/images/svg/open-angle-bracket.svg'
+            image='/images/svg/close-angle-bracket.svg'
             color={primary}
             bgColor={white}
             customInnerClass='about-content'
