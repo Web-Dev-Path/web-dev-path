58d593d49caa1daed86010e2455200dff66b6e1d
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-08-29 11:16:41 +0400

    installed react-hook-form

--- pages/ContactUsForm.js
+++ pages/ContactUsForm.js
@@ -1,7 +1,6 @@
+import { useForm } from 'react-hook-form';
+// import contactUsFormStyles from '@/styles/Contact.module.scss';
+
 export default function ContactUsForm() {
-    return (
-        <div>
-            Contact us form here
-        </div>
-    )
+  return <section>Contact us form here</section>;
 }
