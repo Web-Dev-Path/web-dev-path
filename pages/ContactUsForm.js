f8a713084359b54cc87d70dd6d979a67357b51eb
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-08-31 17:29:16 +0400

    styled subscribe section

--- pages/ContactUsForm.js
+++ pages/ContactUsForm.js
@@ -21,46 +21,62 @@ export default function ContactUsForm() {
 
   return (
     <section className={contactUsFormStyles.contact}>
-        <Container>
-      <form onSubmit={handleSubmit(onSubmit)} className={contactUsFormStyles.contact__form}>
-        <input
-          type='text'
-          placeholder='name'
-          {...register('Name', { required: 'This is required', maxLength: 80 })}
-          className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__name}`}
-        />
-        <p>{errors.Name?.message}</p>
-        <input
-          type='email'
-          placeholder='email'
-          {...register('Email', {
-            required: 'This is required',
-            pattern: /^\S+@\S+$/i,
-          })}
-          className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__email}`}
-        />
-        <p>{errors.Email?.message}</p>
-        <input
-          type='text'
-          placeholder='subject'
-          {...register('Subject', { required: 'This is required' })}
-          className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__subject}`}
-        />
-        <p>{errors.Subject?.message}</p>
-        <textarea
-          {...register('Message', { required: 'This is required' })}
-          placeholder='Write your message here'
-          className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__message}`}
-        />
-        <p>{errors.Message?.message}</p>
-        <input
-          type='checkbox'
-          placeholder='Subscribe to our DevNews!'
-          {...register('Subscribe', {})}
-        />
-        Subscribe to our DevNews!
-        <button className={contactUsFormStyles.contact__button} type='submit' href='/'>Submit</button>
-      </form>
+      <Container>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={contactUsFormStyles.contact__form}
+        >
+          <input
+            type='text'
+            placeholder='name'
+            {...register('Name', {
+              required: 'This is required',
+              maxLength: 80,
+            })}
+            className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__name}`}
+          />
+          <p>{errors.Name?.message}</p>
+          <input
+            type='email'
+            placeholder='email'
+            {...register('Email', {
+              required: 'This is required',
+              pattern: /^\S+@\S+$/i,
+            })}
+            className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__email}`}
+          />
+          <p>{errors.Email?.message}</p>
+          <input
+            type='text'
+            placeholder='subject'
+            {...register('Subject', { required: 'This is required' })}
+            className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__subject}`}
+          />
+          <p>{errors.Subject?.message}</p>
+          <textarea
+            {...register('Message', { required: 'This is required' })}
+            placeholder='Write your message here'
+            className={`${contactUsFormStyles.contact__input} ${contactUsFormStyles.contact__message}`}
+          />
+          <p>{errors.Message?.message}</p>
+          <div className={contactUsFormStyles.contact__subscribe}>
+            <input
+              className={contactUsFormStyles.contact__subscribeInput}
+              type='checkbox'
+              placeholder='Subscribe to our DevNews!'
+              {...register('Subscribe', {})}
+            />
+            Subscribe to our DevNews!
+          </div>
+
+          <button
+            className={contactUsFormStyles.contact__button}
+            type='submit'
+            href='/'
+          >
+            Submit
+          </button>
+        </form>
       </Container>
     </section>
   );
