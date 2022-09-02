d52e0459ee860925aa6ab31d02b2613cdb353358
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-09-02 17:21:50 +0400

    working on positioning

--- pages/contact.js
+++ pages/contact.js
@@ -4,10 +4,10 @@ import contactUsFormStyles from '@/styles/Contact.module.scss';
 export default function ContactUs() {
   return (
     <div className={contactUsFormStyles.contact}>
-      <div className={contactUsFormStyles.formAndDecorations}>
-        <img src='/images/svg/yellow-bracket.svg' />
+      <div className={contactUsFormStyles.contact__formAndDecorations}>
+        <img src='/images/svg/yellow-bracket.svg' className={contactUsFormStyles.contact__yellowBracket} />
         <ContactUsForm />
-        <img src='/images/svg/yellow-colon.svg' />
+        <img src='/images/svg/yellow-colon.svg'  className={contactUsFormStyles.contact__yellowColon} />
       </div>
     </div>
   );
