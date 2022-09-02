f1eef678782e0eec4f57235c80305481df170e84
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-09-02 16:34:15 +0400

    positioned form

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,14 @@
-import ContactUsForm from "./ContactUsForm"
-import contactUsFormStyles from "@/styles/Contact.module.scss"
+import ContactUsForm from './ContactUsForm';
+import contactUsFormStyles from '@/styles/Contact.module.scss';
 
 export default function ContactUs() {
   return (
-  <div className={contactUsFormStyles.contact}>
-    <img src='/images/svg/yellow-bracket.svg' />
-    <ContactUsForm />
-    <img src='/images/svg/yellow-colon.svg' />
-  </div>
+    <div className={contactUsFormStyles.contact}>
+      <div className={contactUsFormStyles.formAndDecorations}>
+        <img src='/images/svg/yellow-bracket.svg' />
+        <ContactUsForm />
+        <img src='/images/svg/yellow-colon.svg' />
+      </div>
+    </div>
   );
 }
