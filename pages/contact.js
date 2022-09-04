f70aae63d82d515c96d6365c9130575a11d7a443
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-09-04 15:43:24 +0400

    added bracket component

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,18 @@
 import ContactUsForm from './ContactUsForm';
 import contactUsFormStyles from '@/styles/Contact.module.scss';
+import Bracket from '@/components/decorations/Bracket';
 
 export default function ContactUs() {
   return (
     <div className={contactUsFormStyles.contact}>
       <div className={contactUsFormStyles.contact__formAndDecorations}>
-        <img src='/images/svg/yellow-bracket.svg' className={contactUsFormStyles.contact__yellowBracket} />
+        <Bracket className={contactUsFormStyles.contact__yellowBracket} />
+        {/* <img src='/images/svg/yellow-bracket.svg' className={contactUsFormStyles.contact__yellowBracket} /> */}
         <ContactUsForm />
-        <img src='/images/svg/yellow-colon.svg'  className={contactUsFormStyles.contact__yellowColon} />
+        <img
+          src='/images/svg/yellow-colon.svg'
+          className={contactUsFormStyles.contact__yellowColon}
+        />
       </div>
     </div>
   );
