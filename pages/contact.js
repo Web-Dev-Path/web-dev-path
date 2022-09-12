c493ec7debcc509442614c4b5bc3940bac4be643
Emerson Mellado <emersonmellado@gmail.com>
2022-09-11 20:01:47 -0700

    testing branch locally

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,23 @@
+import ContactUsForm from '../components/ContactUsForm';
+import contactUsFormStyles from '@/styles/Contact.module.scss';
+import Bracket from '@/components/decorations/Bracket';
+
 export default function ContactUs() {
   return (
-    <div className='contact-us'>
-      <h2>Contact Us</h2>
-      <p>We're building this page.</p>
-      <p>Stay tuned!</p>
+    <div className={contactUsFormStyles.contact}>
+      <div className={contactUsFormStyles.contact__formAndDecorations}>
+        <Bracket className={contactUsFormStyles.contact__yellowBracket} />
+        <ContactUsForm
+        // customClassName={contactUsFormStyles.contact__button}
+        // type='submit'
+        // link='/'
+        // linkText='Submit'
+        />
+        <img
+          src='/images/svg/yellow-colon.svg'
+          className={contactUsFormStyles.contact__yellowColon}
+        />
+      </div>
     </div>
   );
 }
