d300e8936bed84722a7e9fcf22ebd749c12e136c
Cheryl M <webmaster@cherylli.com>
2022-11-18 13:59:55 +1100

    remove extra code

--- components/ContactUsCards.js
+++ components/ContactUsCards.js
@@ -33,28 +33,7 @@ export default function ContactUsCards() {
   return (
     <div className={contactCardStyles.contactCards}>
       <RevealContentContainer>
-        <CardsColumns
-          cards={cards}
-          titles={['FAQ', 'Blog', 'Repository']}
-          images={[
-            '/images/svg/faq-icon.svg',
-            '/images/svg/blog-icon.svg',
-            '/images/svg/repository-icon.svg',
-          ]}
-          altTags={['FAQ', 'Blog', 'Repository']}
-          content={[
-            'Find the main questions about the project.',
-            'Share your projects and ideas with our community.',
-            'Check out our codebase and guidelines.',
-          ]}
-          links={[
-            'https://github.com/Web-Dev-Path/web-dev-path/wiki/FAQ',
-            '/blog',
-            'https://github.com/Web-Dev-Path/web-dev-path',
-          ]}
-          linkText={['Our FAQ', 'Our Blog', 'Our Repository']}
-          customClass='contact-cards'
-        />
+        <CardsColumns cards={cards} customClass='contact-cards' />
       </RevealContentContainer>
     </div>
   );
