e8028786ce80bb10f0bf03b808688c8fa439220a
Thorrell Turner <thorrellt@gmail.com>
2023-04-01 14:22:16 -0400

    finish implementing mixins
    added content parameters so mixins now function as expected

--- styles/_mixins.js
+++ styles/_mixins.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 import {
   $smMobileBreakpoint,
   $mobileBreakpoint,
-  $lgMobileBreakpoin,
+  $lgMobileBreakpoint,
   $tabletBreakpoint,
   $desktopBreakpointMinus,
   $desktopBreakpoint,
@@ -20,50 +20,50 @@ export const transition = $args => css`
   transition: ${$args};
 `;
 
-export const tablet = () => css`
-  @media (min-width: $tabletBreakpoint) {
-    @content;
+export const tablet = content => css`
+  @media (min-width: ${$tabletBreakpoint}) {
+    ${content}
   }
 `;
 
-export const desktop = () => css`
-  @media (min-width: $desktopBreakpoint) {
-    @content;
+export const desktop = content => css`
+  @media (min-width: ${$desktopBreakpoint}) {
+    ${content}
   }
 `;
 
-export const desktopBreakpointPlus = () => css`
-  @media (min-width: $desktopBreakpointPlus) {
-    @content;
+export const desktopBreakpointPlus = content => css`
+  @media (min-width: ${$desktopBreakpointPlus}) {
+    ${content}
   }
 `;
 
-export const mediumDesktop = () => css`
-  @media (min-width: $mediumDesktopBreakpoint) {
-    @content;
+export const mediumDesktop = content => css`
+  @media (min-width: ${$mediumDesktopBreakpoint}) {
+    ${content}
   }
 `;
 
-export const largeDesktop = () => css`
-  @media (min-width: $largeDesktopBreakpoint) {
-    @content;
+export const largeDesktop = content => css`
+  @media (min-width: ${$lgDesktopBreakpoint}) {
+    ${content}
   }
 `;
 
-export const desktopBreakpointMinus = () => css`
-  @media (max-width: $desktopBreakpointMinus) {
-    @content;
+export const desktopBreakpointMinus = content => css`
+  @media (max-width: ${$desktopBreakpointMinus}) {
+    ${content}
   }
 `;
 
-export const mobile = () => css`
-  @media (max-width: $lgMobileBreakpoint) {
-    @content;
+export const mobile = content => css`
+  @media (max-width: ${$lgMobileBreakpoint}) {
+    ${content}
   }
 `;
 
-export const smallMobile = () => css`
-  @media (max-width: $smMobileBreakpoint) {
-    @content;
+export const smallMobile = content => css`
+  @media (max-width: ${$smMobileBreakpoint}) {
+    ${content}
   }
 `;
