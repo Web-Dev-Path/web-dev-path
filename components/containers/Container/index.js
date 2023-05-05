ad1919d7127a923e87b1a68336bc5aae4c872eed
Thorrell Turner <thorrellt@gmail.com>
2023-05-05 13:31:29 -0400

    remove customClass from Containers

--- components/containers/Container/index.js
+++ components/containers/Container/index.js
@@ -1,16 +1,8 @@
 import S from './styles';
 
-export default function Container({
-  className,
-  customClass,
-  children,
-  styles,
-}) {
+export default function Container({ className, children, styles }) {
   return (
-    <S.Container
-      className={customClass ? `${className} ${customClass}` : className}
-      style={styles}
-    >
+    <S.Container className={className} style={styles}>
       {children}
     </S.Container>
   );
