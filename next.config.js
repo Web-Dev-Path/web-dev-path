53c6536d78bf90220b0af2c20efc6f2f773618f5
Cheryl M <webmaster@cherylli.com>
2023-05-12 12:08:57 +1000

    remove styled-components parts in next.config.js

--- next.config.js
+++ next.config.js
@@ -18,26 +18,4 @@ module.exports = withPWA({
       },
     ],
   },
-  compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents:
-      true |
-      {
-        // Enabled by default in development, disabled in production to reduce file size,
-        // setting this will override the default for all environments.
-        displayName: true,
-        // Enabled by default.
-        ssr: true,
-        // Enabled by default.
-        fileName: true,
-        // Empty by default.
-        topLevelImportPaths: '',
-        // Defaults to ["index"].
-        meaninglessFileNames: '',
-        // Enabled by default.
-        cssProp: true,
-        // Empty by default.
-        namespace: '',
-      },
-  },
 });
