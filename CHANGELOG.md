05b1dc6e6162e6a045a1d5707925108862cf80cc
Cheryl M <webmaster@cherylli.com>
2023-11-07 21:18:44 +1100

    add netlify text and icon to the footer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -114,6 +114,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Added .env.development to .gitignore
 - Added Tony to 'about us'
 - Added Igor to 'about us'
+- Added netlify icon and text to the footer
 
 ### Fixed
 
--- components/layout/Footer/index.js
+++ components/layout/Footer/index.js
@@ -50,11 +50,19 @@ export default function Footer() {
           </div>
         </S.NavSocialsContainer>
       </S.Inner>
-      <Container>
+      <S.BottomText>
         <S.Copyright>
           © Web Dev Path {new Date().getFullYear()}. All rights reserved.
         </S.Copyright>
-      </Container>
+        <S.Netlify>
+          This site is powered by
+          <Image
+            src='images/svg/logo-netlify-small-fullcolor-darkmode.svg'
+            width={50}
+            height={50}
+          />
+        </S.Netlify>
+      </S.BottomText>
     </S.Footer>
   );
 }
