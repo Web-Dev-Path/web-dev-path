bc12090af158c1c0f756c73b0caec2249b1fea55
Mariana Caldas <marianacaldassouza@gmail.com>
2023-11-27 21:25:27 -0800

    fix charSet property reference

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charset='utf-8' />
+          <meta charSet='utf-8' />
           <meta name='description' content='Level up your tech career.' />
           <meta name='keywords' content='Web Dev Path' />
           <meta property='og:title' content='Web Dev Path' />
