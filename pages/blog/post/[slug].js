b0c3b07063bf411c700890e8d6c4c5d710aa02a8
satoshi-sh <satoss1108@gmail.com>
2024-06-22 08:08:53 -0500

    Utilize client side rendering instead of server side rendering.

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -1,54 +1,50 @@
-import BlogPostContainer from '@/components/blog/BlogPostContainer';
-import { blogRevalidate } from '@/utils/config';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import BlogPostContainer from '@/components/blog/BlogPostContainer';
 
-const BlogPost = ({ post }) => {
+const BlogPost = () => {
   const router = useRouter();
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
-  return <BlogPostContainer post={post} />;
-};
+  const { slug } = router.query;
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default BlogPost;
+  useEffect(() => {
+    if (!slug) return;
 
-export async function getStaticPaths() {
-  const res = await fetch('https://dev.to/api/articles?username=wdp');
-  const posts = await res.json();
-  const paths = posts.map(post => ({
-    params: { slug: post.slug },
-  }));
-
-  return { paths, fallback: true };
-}
-
-export async function getStaticProps({ params }) {
-  try {
-    const postRes = await fetch(
-      `https://dev.to/api/articles/wdp/${params.slug}`
-    );
-    const post = await postRes.json();
-    const userRes = await fetch(
-      `https://dev.to/api/users/${post.user.user_id}`
-    );
-    const userData = await userRes.json();
-    return {
-      props: {
-        post: {
+    const fetchData = async () => {
+      try {
+        const postRes = await fetch(`https://dev.to/api/articles/wdp/${slug}`);
+        const post = await postRes.json();
+        const userRes = await fetch(`https://dev.to/api/users/${post.user_id}`);
+        const userData = await userRes.json();
+        setPost({
           slug: post.slug,
           title: post.title,
           cover_image: post.cover_image,
           published_at: post.published_at,
           user: userData,
           body_html: post.body_html,
-        },
-      },
-      revalidate: blogRevalidate,
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setPost(null);
+      } finally {
+        setLoading(false);
+      }
     };
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return {
-      notFound: true,
-    };
+
+    fetchData();
+  }, [slug]);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-}
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return <BlogPostContainer post={post} />;
+};
+
+export default BlogPost;
