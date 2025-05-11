1780329373021c8e74b5065588839f130c238e6c
Lucas Assuncao da Silva <lusilvadev@Lucass-MacBook-Air.local>
2025-05-10 19:51:17 -0700

    swiper updated

--- components/containers/CardColumns/CardColumnsInterface.js
+++ components/containers/CardColumns/CardColumnsInterface.js
@@ -1,5 +1,5 @@
-import { SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
