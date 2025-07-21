ea63b347a360ffec0212b1fa00d37883a2757823
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-07-21 11:35:01 +0100

    style: Convert style-components to scss

--- components/containers/Row/index.js
+++ components/containers/Row/index.js
@@ -1,9 +1,9 @@
-import S from './styles';
+import styles from './Row.module.scss';
 
 export function Row({ customClass, children }) {
-  return <S.Row>{children}</S.Row>;
+  return <div className={styles.row}>{children}</div>;
 }
 
 export function RowAlignLeft({ customClass, children }) {
-  return <S.RowAlignLeft>{children}</S.RowAlignLeft>;
+  return <div className={styles.rowAlignLeft}>{children}</div>;
 }
