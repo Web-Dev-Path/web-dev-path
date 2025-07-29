94c56f168f1d619ff01b7c5c9dcf90182b7be9fc
satoshi-sh <satoss1108@gmail.com>
2025-07-29 08:09:55 -0500

    Created comibineClasses function to clean up conditional class handling

--- components/containers/Wrapper/index.js
+++ components/containers/Wrapper/index.js
@@ -1,8 +1,7 @@
 import styles from './Wrapper.module.scss';
+import { combineClasses } from '@/utils/classnames';
 
 export default function Wrapper({ customClass, children }) {
-  const wrapperClass = customClass
-    ? `${styles.wrapper} ${styles[customClass]}`
-    : styles.wrapper;
+  const wrapperClass = combineClasses(styles.wrapper, customClass, styles);
   return <div className={wrapperClass}>{children}</div>;
 }
