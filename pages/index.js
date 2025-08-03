bc35bf5a6b676b17e6ea62bc15181fec3b012a4b
satoshi-sh <satoss1108@gmail.com>
2025-08-03 07:44:51 -0500

    Migrated Bracket to scss

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import TwoColumn from '@/components/containers/TwoColumn';
 import { CardsColumns } from '@/components/containers/CardColumns';
-import S from '@/styles/pages/homeStyles';
+import Bracket from '@/components/decorations/Bracket';
+import bracketStyles from '@/components/decorations/Bracket/Bracket.module.scss';
 import RevealContentContainer from '@/components/containers/RevealContentContainer';
 import { useTheme } from 'styled-components';
 
@@ -21,7 +22,7 @@ export default function Home() {
       </RevealContentContainer>
 
       <RevealContentContainer>
-        <S.Bracket />
+        <Bracket className={bracketStyles.homeBracket} />
         <TwoColumn
           title='Get involved.'
           content='Web Dev Path runs on volunteers. Here are the ways you can get involved with us:'
@@ -66,8 +67,6 @@ export default function Home() {
       </RevealContentContainer>
 
       <RevealContentContainer>
-        <S.Stick />
-
         <TwoColumn
           title='Nonprofit?'
           content='Web Dev Path can help your nonprofit with web projects of various sizes. Connect with us to find out how.'
--- styles/pages/homeStyles.js
+++ styles/pages/homeStyles.js
@@ -1,24 +1,6 @@
 import styled, { css } from 'styled-components';
-import Bracket from '@/components/decorations/Bracket';
 import Stick from '@/components/decorations/Stick';
 
-const HomeBracket = styled(Bracket)`
-  left: 0;
-  position: absolute;
-  transform: translate(10rem, -5rem);
-
-  ${props => css`
-    @media (min-width: ${props.theme.breakpoints.desktopPlus}) {
-      transform: translate(50%, -5rem);
-      width: 30%;
-    }
-  `}
-  ${props => css`
-    @media (max-width: ${props.theme.breakpoints.desktopMinus}) {
-      display: none;
-    }
-  `}
-`;
 const HomeStick = styled(Stick)`
   right: 0;
   position: absolute;
@@ -38,4 +20,4 @@ const HomeStick = styled(Stick)`
   `}
 `;
 
-export default { Bracket: HomeBracket, Stick: HomeStick };
+export default { Stick: HomeStick };
