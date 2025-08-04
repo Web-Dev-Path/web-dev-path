c096f6eaf003953cf8bca256006179b5cb011146
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-08-04 18:23:52 +0100

    style: Prevent horizontal scroll caused by overflowing long titles

--- components/snippets/Title.js
+++ components/snippets/Title.js
@@ -1,5 +1,7 @@
+const styleProp = { overflowWrap: 'break-word', hyphens: 'auto' };
+
 const Title = ({ title }) => {
-  return <h2>{title}</h2>;
+  return <h2 style={styleProp}>{title}</h2>;
 };
 
 export default Title;
