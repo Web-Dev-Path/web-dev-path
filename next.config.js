1c65e77166162ce31ff8b9754c0a9d4bd0827604
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2026-06-10 01:01:50 +0100

    chore: Remove next-pwa implementation
    The package is no longer actively maintained

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   compiler: {
     styledComponents: {
       ssr: true,
@@ -29,4 +22,6 @@ module.exports = withPWA({
       },
     ],
   },
-});
+};
+
+module.exports = nextConfig;
